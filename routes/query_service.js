c010e069115b334ed4738446cb08f47d8bcae57b
James E. Marca <jmarca@translab.its.uci.edu>
2013-03-20 12:23:59 -0700

    test the query service.  passes

--- routes/query_service.js
+++ routes/query_service.js
@@ -1,13 +1,11 @@
-var calvad_querier = require('./query_couch')
-var get_time = require('./get_time').get_time
+var calvad_querier = require('../lib/query_couch')
+var get_time = require('../lib/get_time').get_time
 function query_service(app,prefix){
     if (prefix === undefined) prefix = ''
     // need to handle trailing slash properly
-
-    app.get(prefix+'/:spatialagg/:aggregate/:year/:detector_id.json'
+    app.get('/'+prefix+'/:spatialagg/:aggregate/:year/:detector_id.json'
            ,function(req,res,next){
                 // build the feature to extract
-
                 var start_end = get_time(req)
                 var feature = {'properties':{}}
                 feature.properties.ts = start_end.start.getTime()/1000
@@ -20,3 +18,4 @@ function query_service(app,prefix){
            )
 
 }
+exports.query_service=query_service
