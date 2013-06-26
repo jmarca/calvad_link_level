2d13e2a80f0299fae95911a39ea3f6022a191864
James E. Marca <jmarca@translab.its.uci.edu>
2013-06-26 14:12:06 -0700

    update for newer reducer library, outliers paths work
    checked tests are passing and are properly calling the view that grabs
    data that do not have outliers.

--- test/test_couch_fetch.js
+++ test/test_couch_fetch.js
@@ -6,7 +6,7 @@ var async = require('async')
 var _ = require('lodash')
 
 var couch_cacher= require('calvad_couch_cacher')
-var reducer= require('calvad_reducer')
+var reducer= require('calvad_reducer').reducer
 
 
 var ts = new Date(2008,6,25,13,0).getTime()/ 1000
@@ -15,10 +15,10 @@ var endts =  new Date(2008,6,25,15,0).getTime()/1000
 describe('couchCache get',function(){
     describe('monthly, by detector', function(){
         it('can get something',function(done){
-            var accum = reducer();
+            var accum = reducer({'time_agg':'monthly'
+                                ,'spatial_agg':sagg});
             var sagg = 'detector'
-            var cacher = couch_cacher.couchCache({'time_agg':'monthly'
-                                      ,'spatial_agg':sagg})
+            var cacher = couch_cacher.couchCache()
 
             var getter = cacher.get(accum.process_collated_record)
             async.parallel([function(cb){
